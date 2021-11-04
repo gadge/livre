85c4fd1098b3614028038eca443e7ae2b1536c59
Hoyeung <hoyeungw@outlook.com>
2021-11-04 12:03:28 +0800

    add test

--- packages/design-space/test/url.replace.test.js
+++ packages/design-space/test/url.replace.test.js
@@ -1,40 +1,37 @@
-const { parsePath } = require('@acq/path')
-const { decoEntries, logger, decoString } = require('@spare/logger')
-const urls = [
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city-hero-852x479.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_7-852x1278.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_3-852x682.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_9-852x1278.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_16-852x568.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_5-852x568.jpg',
-  'https://static.dezeen.com/uploads/2021/11/site-plan-agua-210-hemaa-architecture-mexico-city-drawings_dezeen_2364_col_3.jpg',
-  'https://static.dezeen.com/uploads/2021/11/basement-agua-210-hemaa-architecture-mexico-city-drawings_dezeen_2364_col_4.jpg',
-  'https://static.dezeen.com/uploads/2021/11/ground-floor-agua-210-hemaa-architecture-mexico-city-drawings_dezeen_2364_col_0.jpg',
-  'https://static.dezeen.com/uploads/2021/11/first-floor-agua-210-hemaa-architecture-mexico-city-drawings_dezeen_2364_col_1-1.jpg',
-  'https://static.dezeen.com/uploads/2021/11/roof-garden-agua-210-hemaa-architecture-mexico-city-drawings_dezeen_2364_col_2.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_0-scaled.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_1-scaled.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_6.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_13-scaled.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_14-scaled.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_15-scaled.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_0-1-scaled.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_1-1.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_5-1-scaled.jpg',
-  'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_8-scaled.jpg',
-]
+import { parsePath }                     from '@acq/path'
+import { decoEntries, decoString, says } from '@spare/logger'
+import { makeReplaceable }               from '@spare/translator'
+import { mapEntries }                    from '@vect/object-mapper'
 
-const regex = /(?<=[-_]col_\d+|hero|sq)(-\d+|x\d+|-scaled){1,2}\.jpg$/
 
-const test = () => {
-  const results = urls.map(url => {
-    const path = parsePath(url)
-    const result = url.replace(regex, '.jpg')
-    const path2 = parsePath(result)
-    return [ decoString(path.base + path.ext), decoString(path2.base + path2.ext) ]
-  })
-  decoEntries(results) |> logger
+const URLS = {
+  col_n: 'https://static.dezeen.com/uploads/2021/11/ground-floor-agua-210-hemaa-architecture-mexico-city-drawings_dezeen_2364_col_0.jpg',
+  col_n_n: 'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_1-1.jpg',
+  col_n_mx: 'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_5-852x568.jpg',
+  col_n_n_mx: 'https://static.dezeen.com/uploads/2021/05/leckie-studio-penthouse-big-skyscraper-vancouver_dezeen_2364_col_4-1-852x1065.jpg',
+  col_scale: 'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_13-scaled.jpg',
+  col_n_scale: 'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city_dezeen_2364_col_0-1-scaled.jpg',
+  hero_mx: 'https://static.dezeen.com/uploads/2021/11/agua-210-hemaa-architecture-mexico-city-hero-852x479.jpg',
+  hero_n_mx: 'https://static.dezeen.com/uploads/2021/04/reception-room-alcove-nanometer-architecture_dezeen_2364_hero_1-852x479.jpg',
+  hero_n2_mx: 'https://static.dezeen.com/uploads/2021/01/clayworks-interior-finishes_dezeen_hero1-852x479.jpg',
+  sq_n_mx: 'https://static.dezeen.com/uploads/2021/01/clayworks-interior-finishes_dezeen_2364_sq-1-852x852.jpg',
 }
 
-test()
+const REG_DEZEEN_REVIVE = /(?<=[_-]col_\d+|hero\d?|sq\d?)([_-]\d+|x\d+|-scaled){1,3}\.jpg$/
+const REG_DEZEEN_NAMING = /[_-]*dezeen_\d+_col[_-]*/
+const REG_DEZEEN_FOLDER = /([_-]*dezeen_\d+)?[_-](col_\d+|hero\d?|sq\d?)([_-]\d+|x\d+|-scaled){0,3}\.jpg$/
+
+const test = ({ dict, theme }) => {
+  const curr_to_next = mapEntries(URLS, ([ key, url ]) => {
+    const curr = parsePath(url)
+    const nextUrl = url.replace(dict)
+    const next = parsePath(nextUrl)
+    return [ decoString(curr.base + curr.ext), decoString(next.base + next.ext) ]
+  })
+  decoEntries(Object.entries(curr_to_next)) |> says[theme]
+}
+
+test({ dict: [ [ REG_DEZEEN_REVIVE, '.jpg' ] ] |> makeReplaceable, theme: 'revive' })
+test({ dict: [ [ REG_DEZEEN_REVIVE, '.jpg' ], [ REG_DEZEEN_NAMING, '-' ] ] |> makeReplaceable, theme: 'naming' })
+test({ dict: [ [ REG_DEZEEN_FOLDER, '' ] ] |> makeReplaceable, theme: 'folder' })
 
