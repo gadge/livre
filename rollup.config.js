481c91442a3ea074df3bbb76d41664c8dcb213be
Hoyeung <hoyeungw@outlook.com>
2021-11-24 21:46:13 +0800

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -28,16 +28,10 @@ export default [
         exclude: 'node_modules/**',
         babelHelpers: 'bundled',
         presets: [
-          [ '@babel/preset-env', { targets: { node: '14' }, loose: true } ]
+          [ '@babel/preset-env', { targets: { node: '16' }, loose: true } ]
         ],
         plugins: [
-          // ['@babel/plugin-proposal-decorators', { legacy: true }],
-          // [ '@babel/plugin-proposal-optional-chaining' ],
-          // [ '@babel/plugin-proposal-nullish-coalescing-operator' ],
           [ '@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' } ],
-          [ '@babel/plugin-proposal-class-properties', { loose: true } ],
-          // [ '@babel/plugin-proposal-private-methods', { loose: true } ],
-          // [ '@babel/plugin-transform-runtime', { helpers: false, } ]
         ]
       }),
       json(),
